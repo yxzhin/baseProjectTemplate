a0e30ebf35f448ac10d872943ec9955554f25bd0
yxzhin <yxzhin@gmail.com>
2026-01-29 19:24:51 +0100

    feat: add balance, rank && inventory columns to users table; generate alembic migration

--- src/server/app/db/models/user.py
+++ src/server/app/db/models/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, String, func
+from sqlalchemy import DateTime, Integer, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ...db import Base
@@ -18,6 +18,9 @@ class User(Base):
     discord_id: Mapped[int] = mapped_column(nullable=False, unique=True, index=True)
     username: Mapped[str] = mapped_column(String(255), nullable=False)
     avatar_url: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    balance: Mapped[int] = mapped_column(Integer(), nullable=False, default=0)
+    rank: Mapped[int] = mapped_column(Integer(), nullable=False, default=0)
+    inventory: Mapped[str | None] = mapped_column(String(255), nullable=True)
 
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
